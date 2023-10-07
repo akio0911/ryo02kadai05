ebabd43c895c6a5fe388a960933e072d839ee7cc
ryosuke <kolor0211@gmail.com>
2023-10-07 16:39:18 +0900

    first commit

---
+++ ryo02Kadai5/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  ryo02Kadai5
+//
+//  Created by 大田涼介 on 2023/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var displayleftNumber = ""
+    @State var displayrightNumber = ""
+    @State var resultNumber = 0.0
+    @State var showAlert = false
+    @State var alertMessage = ""
+    
+    var body: some View {
+        VStack {
+            VStack {
+                HStack {
+                    TextFieldView(displayNumber: $displayleftNumber)
+                    Text("÷")
+                    TextFieldView(displayNumber: $displayrightNumber)
+                }
+                Button {
+                    calculate()
+                } label: {
+                    Text("計算")
+                }
+                .alert("課題5", isPresented: $showAlert) {
+                    Button("OK") {
+                    }
+                } message: {
+                    Text(alertMessage)
+                }
+            }
+            .padding()
+            HStack {
+                Text(String(resultNumber))
+            }
+        }
+    }
+    
+    func calculate() {
+        guard let leftNumber = Double(displayleftNumber) else {
+            showAlert = true
+            alertMessage  = "割られる数を入力してください"
+            return
+        }
+        
+        guard let rightNumber = Double(displayrightNumber) else {
+            showAlert = true
+            alertMessage = "割られる数を入力してください"
+            return
+        }
+        
+        guard leftNumber != 0 else {
+            showAlert = true
+            alertMessage = "割る数には0を入力しないでください"
+            return
+        }
+        
+        guard rightNumber != 0 else {
+            showAlert = true
+            alertMessage = "割る数には0を入力しないでください"
+            return
+        }
+        
+        resultNumber = leftNumber / rightNumber
+    }
+    
+}
+
+struct TextFieldView: View {
+    @Binding var displayNumber: String
+    var body: some View {
+        TextField("", text: $displayNumber)
+            .keyboardType(.numberPad)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ryo02Kadai5/ryo02Kadai5App.swift
@@ -0,0 +1,17 @@
+//
+//  ryo02Kadai5App.swift
+//  ryo02Kadai5
+//
+//  Created by 大田涼介 on 2023/10/07.
+//
+
+import SwiftUI
+
+@main
+struct ryo02Kadai5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
